b5ab78101fa419391dd35b3281f570422a5db808
l-papadopol <l.papadopol@campus.uniurb.it>
2025-03-31 11:43:46 +0200

    JavaDOC :penguin:

---
+++ docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"controller"},{"l":"controller.ai"},{"l":"controller.handlers"},{"l":"main"},{"l":"model"},{"l":"model.builders"},{"l":"model.entities"},{"l":"test"},{"l":"view"},{"l":"view.components"}];updateSearchResults();
